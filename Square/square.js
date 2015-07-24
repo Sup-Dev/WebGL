46eff4773de23eb62a09225e15a6113b19b8d00b
Sourabh Dev <sourabh_dev@hotmail.com>
2015-07-24 08:33:37 +0530

    Configured WebGL in square.js

--- Square/square.js
+++ Square/square.js
@@ -23,4 +23,14 @@ window.onload = function init() {
     ];
 
     // alternate var vertices = [vec2(-0.5, -0.5), vec2(-0.5, 0.5), vec2(0.5, 0.5), vec2(0.5, 0.5)];
+
+    // Configure WebGL
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Load shaders and initialize attribute buffers
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 }
